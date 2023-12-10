add05ca9a5574791454295a3dda8b0e5f4c5f8ce
kuma <kumatiki@mac.com>
2023-12-10 20:53:48 +0900

    auto commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,6 +10,23 @@ st.text('本文のテストです。本文はここです')
 
 st.link_button("Netlify page", "https://kumadata.netlify.app/")
 
+st.text('ネイピア数を底とする指数関数')
+fig. ax = plt.subplots()
+
+# xの値（-2～5で0.1刻みで配列生成）
+x=np.arange(-2, 5, 0.1)
+
+#　ネイピア数を底とする関数
+y = np.exp(x)
+
+# グラフの設定
+ax.plot(x, y) # プロット
+ax.set_title('exponential function', fontsize = 12)
+ax.set_xlabel('x', fontsize = 12)
+ax.set_ylabel('f(x)', fontsize = 12)
+ax.grid() # グリッド描画
+st.pyplot(fig)
+
 st.text('シグモイド関数')
 fig, ax = plt.subplots()
 
