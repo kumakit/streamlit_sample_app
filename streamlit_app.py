53b48a3f5afba3cdfa1e2351844aa1d132557ef1
kuma <kumatiki@mac.com>
2023-12-13 23:46:44 +0900

    auto commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,3 +43,37 @@ ax.set_xlabel('x', fontsize = 12)
 ax.set_ylabel('f(x)', fontsize = 12)
 ax.grid() # グリッド描画
 st.pyplot(fig)
+
+st.text('底を２とする対数関数　y=log2(x)')
+fig, ax = plt.subplots()
+
+# xの値（-8～8で0.1刻みで配列生成）
+x = np.arange(0, 8, 0.1)
+
+#　y=log2(x)
+y = np.log2(x)
+
+# グラフの設定
+ax.plot(x, y) # プロット
+ax.set_title('Logarithmic function', fontsize = 12)
+ax.set_xlabel('x', fontsize = 12)
+ax.set_ylabel('f(x)', fontsize = 12)
+ax.grid() # グリッド描画
+st.pyplot(fig)
+
+st.text('底をe(ネイピア数=2.71828183…)とする対数(自然対数)')
+fig, ax = plt.subplots()
+
+# xの値（-8～8で0.1刻みで配列生成）
+x = np.arange(-1, 100, 0.1)
+
+#　シグモイド関数
+y = np.log(x)
+
+# グラフの設定
+ax.plot(x, y) # プロット
+ax.set_title('natural logarithm function', fontsize = 12)
+ax.set_xlabel('x', fontsize = 12)
+ax.set_ylabel('f(x)', fontsize = 12)
+ax.grid() # グリッド描画
+st.pyplot(fig)
