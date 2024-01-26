679632628d7fd4593ae51b42d2f80a731b198fb6
kuma <kumatiki@mac.com>
2024-01-26 23:20:09 +0900

    auto commit

--- main.py
+++ main.py
@@ -3,3 +3,14 @@ import numpy as np
 import pandas as pd
 
 st.title('streamlit 超入門')
+
+st.write('DataFrame')
+
+df = pd.DataFrame({
+    '1列目':[1, 2, 3, 4],
+    '2列目':[10, 20, 30, 40]
+})
+
+st.write(df)
+
+st.table(df.style.highlight_max(axis=0))
