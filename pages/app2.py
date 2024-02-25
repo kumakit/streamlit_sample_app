79062ffb819ebe00aec846862be39d586cde3084
kuma <kumatiki@mac.com>
2024-02-25 17:41:45 +0900

    auto commit

---
+++ pages/app2.py
@@ -0,0 +1,93 @@
+import streamlit as st
+import requests
+import urllib.parse
+import json
+import io
+import time
+
+def synthsize_speech(input_text, speaker='ずんだもん（ノーマル）'):
+
+  speaker_type = {
+      '四国めたん（あまあま）':"0",
+      'ずんだもん（あまあま）':"1",
+      '四国めたん（ノーマル）':"2",
+      'ずんだもん（ノーマル）':'3',
+      '四国めたん（セクシー）':'4',
+      'ずんだもん（セクシー）':'5',
+      '四国めたん（ツンツン）':'6',
+      'ずんだもん（ツンツン）':'7',
+      '春日部つむぎ（ノーマル）':'8',
+      '波音リツ（ノーマル）':'9',
+      '雨晴はう（ノーマル）':'10',
+      '玄野武宏（ノーマル）':'11'
+  }
+
+  # 音素データ生成
+  text = urllib.parse.quote(input_text)
+  response = requests.post("https://api.tts.quest/v3/voicevox/synthesis?text=" + text + "&speaker=" + speaker_type[speaker])
+  res_json = response.json()
+  print(json.dumps(response.json(), indent=4))
+
+  # 音声合成のステータスを確認する
+  res_status = requests.get(res_json["audioStatusUrl"]).json()
+
+  # ステータスがTrueになるまで待機する
+  while res_status["isAudioReady"] is not True:
+      # 一定時間待つ
+      time.sleep(0.5)
+      # ステータスを再度確認する
+      res_status = requests.post(res_json["audioStatusUrl"]).json()
+
+  res_wav = requests.get(res_json["wavDownloadUrl"]).content
+
+  return res_wav
+
+st.title('音声出力アプリ')
+
+st.markdown('### データ準備')
+
+input_option = st.selectbox(
+  '入力データの選択',
+  ('直接入力', 'テキストファイル')
+)
+input_data = None
+
+if input_option =='直接入力':
+  input_data =st.text_area('こちらにテキストを入力してください','VOICEVOX用のサンプル文になります。')
+else:
+  uploaded_file = st.file_uploader('テキストファイルをアップロードしてください。',['txt'])
+  if uploaded_file is not None:
+    content = uploaded_file.read()
+    input_data = content.decode()
+
+if input_data is not None:
+  st.write('入力データ')
+  st.write(input_data)
+  st.markdown('### パラメータ設定')
+
+  speaker = st.selectbox(
+    'VOICEの選択をしてください',
+    ('ずんだもん（あまあま）',
+    '四国めたん（あまあま）',
+    '四国めたん（ノーマル）',
+    'ずんだもん（ノーマル）',
+    '四国めたん（セクシー）',
+    'ずんだもん（セクシー）',
+    '四国めたん（ツンツン）',
+    'ずんだもん（ツンツン）',
+    '春日部つむぎ（ノーマル）',
+    '波音リツ（ノーマル）',
+    '雨晴はう（ノーマル）',
+    '玄野武宏（ノーマル）'
+    )
+  )
+
+  st.markdown('### 音声合成')
+  st.write('こちらの文章で音声ファイルの生成を行いますか？')
+  if st.button('開始'):
+    comment = st.empty()
+    comment.write('音声出力を作成しています')
+    def_response = synthsize_speech(input_data, speaker)
+
+    st.audio(def_response)
+    comment.write('完了しました')
